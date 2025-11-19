e2391496ef6f741d55c36966de16e8eb0ef0bad6
Bohdan <bogdansumy2004@gmail.com>
2025-11-19 22:50:23 +0200

    Added projectName to header component

--- my-app/src/app/components/header/header.component.ts
+++ my-app/src/app/components/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  projectName = 'My Angular Project';
+  projectName = 'SPA Angular Project';  
 }
